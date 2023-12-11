df3b2e1c244765873f17a6a27b002c1b4f205fb5
Dlurak <84224239+Dlurak@users.noreply.github.com>
2023-12-11 18:32:00 +0100

    Refactored Day 11

--- 2023/day11/part.ts
+++ 2023/day11/part.ts
@@ -1,7 +1,5 @@
 import { filterOut, isInRange, loadData, transpose } from '@helper';
 
-const EXPANSION_FACTOR = 1_000_000 - 1;
-
 const input = await loadData({
 	part: 1,
 	day: 11,
@@ -23,10 +21,7 @@ const colsWithoutHash = filterOut(
 	-1,
 );
 
-const coordinates: {
-	row: number;
-	col: number;
-}[] = [];
+const coordinates: { row: number; col: number }[] = [];
 for (let i = 0; i < matrix.length; i++) {
 	for (let j = 0; j < matrix[i].length; j++) {
 		if (matrix[i][j] === '#')
@@ -38,41 +33,41 @@ for (let i = 0; i < matrix.length; i++) {
 }
 
 // MEASURE DISTANCE
-let distance = 0;
-for (let i = 0; i < coordinates.length; i++) {
-	const currentCoord = coordinates[i];
-	for (let j = i + 1; j < coordinates.length; j++) {
-		const destinationCoord = coordinates[j];
+const measureDistance = (expFactor: number) => {
+	let distance = 0;
+	for (let i = 0; i < coordinates.length; i++) {
+		const currentCoord = coordinates[i];
+		for (let j = i + 1; j < coordinates.length; j++) {
+			const destinationCoord = coordinates[j];
 
-		const rowsExpansion =
-			rowsWithoutHash
-				.map((it) =>
+			const rowsExpansion =
+				rowsWithoutHash.filter((it) =>
 					isInRange(
 						Math.min(destinationCoord.row, currentCoord.row),
 						it,
 						Math.max(destinationCoord.row, currentCoord.row),
 					),
-				)
-				.filter((it) => it).length * EXPANSION_FACTOR;
+				).length * expFactor;
 
-		const colsExpansion =
-			colsWithoutHash
-				.map((it) =>
+			const colsExpansion =
+				colsWithoutHash.filter((it) =>
 					isInRange(
 						Math.min(destinationCoord.col, currentCoord.col),
 						it,
 						Math.max(destinationCoord.col, currentCoord.col),
 					),
-				)
-				.filter((it) => it).length * EXPANSION_FACTOR;
+				).length * expFactor;
 
-		const rowDifferenceRaw =
-			Math.abs(destinationCoord.row - currentCoord.row) + rowsExpansion;
-		const colDifferenceRaw =
-			Math.abs(destinationCoord.col - currentCoord.col) + colsExpansion;
+			const rowDifferenceRaw =
+				Math.abs(destinationCoord.row - currentCoord.row) + rowsExpansion;
+			const colDifferenceRaw =
+				Math.abs(destinationCoord.col - currentCoord.col) + colsExpansion;
 
-		distance += rowDifferenceRaw + colDifferenceRaw;
+			distance += rowDifferenceRaw + colDifferenceRaw;
+		}
 	}
-}
+	return distance;
+};
 
-console.log(distance);
+console.log(`Part 1: ${measureDistance(1)}`);
+console.log(`Part 2: ${measureDistance(1_000_000 - 1)}`);
