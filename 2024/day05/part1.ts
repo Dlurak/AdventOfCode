1c39041edc3b5cfe65ec832842e4a168c2163f31
Dlurak <84224239+Dlurak@users.noreply.github.com>
2024-12-05 19:01:37 +0100

    Change to a graph ds to improve performance

--- 2024/day05/part1.ts
+++ 2024/day05/part1.ts
@@ -4,27 +4,33 @@ const input = await loadData();
 
 const parse = (input: string) => {
 	const [rulesString, instructionsString] = input.split('\n\n', 2);
-	const rules = lines(
-		rulesString,
-		(l) => mapToInt(l.split('|', 2)) as [number, number],
-	);
-	const instructions = lines(instructionsString, (l) => mapToInt(l.split(',')));
-	return { rules, instructions };
+
+	const rules = lines(rulesString, (l) => mapToInt(l.split('|', 2)));
+
+	return {
+		instructions: lines(instructionsString, (l) => mapToInt(l.split(','))),
+		rulesGraph: rules.reduce<Record<number, number[]>>(
+			(acc, [r1, r2]) => ({
+				...acc,
+				[r1]: [...(acc[r1] ?? []), r2],
+			}),
+			{},
+		),
+	};
 };
 
-const { rules, instructions } = parse(input);
+const { instructions, rulesGraph } = parse(input);
 
 const isCorrectlySorted = (instruction: number[]) => {
 	return instruction.every((page, i) => {
 		const followingPages = instruction.slice(i + 1);
-		return followingPages.every(
-			(p) => !rules.some(([r1, r2]) => r1 === p && r2 === page),
-		);
+		return followingPages.every((p) => rulesGraph[page].includes(p));
 	});
 };
 
-const summands = instructions.filter(isCorrectlySorted);
-
 console.log(
-	sumBy(summands, (instruction) => instruction[(instruction.length - 1) / 2]),
+	sumBy(
+		instructions.filter(isCorrectlySorted),
+		(instruction) => instruction[(instruction.length - 1) / 2],
+	),
 );
--- 2024/day05/part2.ts
+++ 2024/day05/part2.ts
@@ -4,59 +4,44 @@ const input = await loadData();
 
 const parse = (input: string) => {
 	const [rulesString, instructionsString] = input.split('\n\n', 2);
-	const rules = lines(
-		rulesString,
-		(l) => mapToInt(l.split('|', 2)) as [number, number],
-	);
-	const instructions = lines(instructionsString, (l) => mapToInt(l.split(',')));
-	return { rules, instructions };
+
+	const rules = lines(rulesString, (l) => mapToInt(l.split('|', 2)));
+
+	return {
+		instructions: lines(instructionsString, (l) => mapToInt(l.split(','))),
+		rulesGraph: rules.reduce<Record<number, number[]>>(
+			(acc, [r1, r2]) => ({
+				...acc,
+				[r1]: [...(acc[r1] ?? []), r2],
+			}),
+			{},
+		),
+	};
 };
 
-const { rules, instructions } = parse(input);
+const { rulesGraph, instructions } = parse(input);
 
-const resort = (instruction: number[]): number[] => {
-	const copiedInstruction = instruction.slice();
-	const { invalidIndex, lastValidIndex } = copiedInstruction.reduce(
-		(acc, page, i, arr) => {
-			if (acc.invalidIndex !== -1) {
-				return acc;
-			}
-
-			const followingPages = arr.slice(i + 1);
-			const toEarlyNumberIndex = followingPages.findIndex((p) => {
-				return rules.some(([r1, r2]) => r1 === p && r2 === page);
-			});
-
-			if (toEarlyNumberIndex !== -1) {
-				const invalidIndex = toEarlyNumberIndex + i + 1;
-				return { ...acc, invalidIndex };
-			}
-
-			return { ...acc, lastValidIndex: i };
-		},
-		{ invalidIndex: -1, lastValidIndex: -1 },
-	);
-
-	if (invalidIndex === -1) {
-		return copiedInstruction;
-	}
-
-	// Doing it with slicing is very slow. We need splice (unpure)
-	const elementToMove = copiedInstruction.splice(invalidIndex, 1)[0];
-	copiedInstruction.splice(lastValidIndex + 1, 0, elementToMove);
-
-	return resort(copiedInstruction);
+const resort = (instruction: number[]) => {
+	return instruction.toSorted((a, b) => {
+		if ((rulesGraph[a] ?? []).includes(b)) {
+			return -1;
+		}
+		if ((rulesGraph[b] ?? []).includes(a)) {
+			return 1;
+		}
+		return 0;
+	});
 };
 
-const result = sumBy(
-	instructions
-		.map((instruction) => {
-			const resorted = resort(instruction);
-			const isModified = instruction.join(',') !== resorted.join(',');
-			return [resorted, isModified] as const;
-		})
-		.filter(([_, isModified]) => isModified),
-	([instruction]) => instruction[(instruction.length - 1) / 2],
+console.log(
+	sumBy(
+		instructions
+			.map((instruction) => {
+				const resorted = resort(instruction);
+				const isModified = instruction.join(',') !== resorted.join(',');
+				return [resorted, isModified] as const;
+			})
+			.filter(([_, isModified]) => isModified),
+		([instruction]) => instruction[(instruction.length - 1) / 2],
+	),
 );
-
-console.log(result);
